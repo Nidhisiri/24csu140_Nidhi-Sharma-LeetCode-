dedf74280edd68b55696b5daa60090b80262795e
Nidhisiri <24csu140@ncuindia.edu>
2025-11-15 14:33:00 +0530

    Create IntersectionOfTwoLinkedLists.java

---
+++ IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        // If no intersection, both pointers will become null at the same time
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+
+        // Either intersection node or null
+        return a;
+    }
+}
